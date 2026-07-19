5d1fd509d7f1ac688bf3113f140ef6f023de6767
SamuelSGSilva <1.22492375e+08+SamuelSGSilva@users.noreply.github.com>
2026-07-19 12:20:58 +0000

    config: autorizar domínio dataflexbr.com para carregamento de imagens externas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import type { RemotePattern } from "next/dist/shared/lib/image-config";
+
+type ImageConfig = {
+  remotePatterns?: RemotePattern[];
+};
+
+type NextConfigWithImages = NextConfig & ImageConfig;
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dataflexbr.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
